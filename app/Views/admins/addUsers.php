80dfa57666e1758e4d23c42922c03ff7aa487320
Muhamad Fauzi <muhamadfz@unsub.ac.id>
2023-12-25 12:31:25 +0700

    Fix Register User

--- app/Views/admins/addUsers.php
+++ app/Views/admins/addUsers.php
@@ -7,7 +7,7 @@
     <h1 class="h3 mb-4 text-gray-800">User Table</h1>
     <div class="row">
         <div class="col-md-8">
-            <form class="user" action="<?= url_to('register') ?>" method="post">
+            <form class="user" action="/Admin/store" method="post">
                 <?= csrf_field() ?>
 
                 <div class="form-group">
--- app/Views/users/profile.php
+++ app/Views/users/profile.php
@@ -7,19 +7,16 @@
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="<?= base_url('/img/' . user()->user_image) ;?>" class="img-fluid rounded-start mt-md-2 " alt="<?= user()->username ;?>">
+                        <img src="<?= base_url('/img/profile/' . $user->user_image) ;?>" class="img-fluid rounded-start mt-md-2 " alt="<?= $user->username ;?>">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Username : <?= user()->username ;?></li>
-                            <?php if(user()->fullname): ?>
-                                <li class="list-group-item">Fullname : <?= user()->fullname ;?></li>
+                            <li class="list-group-item">Username : <?= $user->username ;?></li>
+                            <?php if($user->fullname): ?>
+                                <li class="list-group-item">Fullname : <?= $user->fullname ;?></li>
                             <?php endif; ?>
-                            <li class="list-group-item">Email : <?= user()->email ;?></li>
-                            <li class="list-group-item">
-                                <small><a href="<?= base_url('user/dashboard') ;?>">&laquo Kembali</a></small>
-                            </li>
+                            <li class="list-group-item">Email : <?= $user->email ;?></li>
                         </ul>
                         </div>
                     </div>
