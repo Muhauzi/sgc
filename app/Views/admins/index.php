15dfacd5b2170027d020d871975e0396b9234b49
Farma <54702653+Farma27@users.noreply.github.com>
2023-12-27 21:03:02 +0700

    patch 271220232103
    - fix dashboard layout

--- app/Views/admins/index.php
+++ app/Views/admins/index.php
@@ -1,7 +1,7 @@
 <?= $this->extend('templates/index'); ?>
 <?= $this->section('content'); ?>
 <!-- Begin Page Content -->
-<div class="container-fluid col-md-8 text-center mx-auto">
+<div class="container-fluid col-10 text-center mx-auto">
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">User Table</h1>
