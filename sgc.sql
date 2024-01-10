50f1198249ca171509ecdc39311e2aaeeee995b7
Muhamad Fauzi <115764458+Muhauzi@users.noreply.github.com>
2024-01-11 01:17:00 +0700

    New SQL Database

--- sgc.sql
+++ sgc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jan 2024 pada 10.17
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.2.4
+-- Waktu pembuatan: 10 Jan 2024 pada 19.16
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `auth_groups_users` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `group` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `auth_groups_users`
@@ -41,7 +41,11 @@ CREATE TABLE `auth_groups_users` (
 INSERT INTO `auth_groups_users` (`id`, `user_id`, `group`, `created_at`) VALUES
 (1, 1, 'superadmin', '2023-12-14 11:16:21'),
 (13, 18, 'pedagang', '2023-12-29 04:17:46'),
-(14, 19, 'user', '2023-12-29 04:24:08');
+(14, 19, 'user', '2023-12-29 04:24:08'),
+(17, 21, 'user', '2024-01-04 05:56:43'),
+(18, 21, 'admin', '2024-01-04 05:56:43'),
+(21, 23, 'pedagang', '2024-01-10 14:44:16'),
+(22, 24, 'pedagang', '2024-01-10 14:45:18');
 
 -- --------------------------------------------------------
 
@@ -62,16 +66,19 @@ CREATE TABLE `auth_identities` (
   `last_used_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `auth_identities`
 --
 
 INSERT INTO `auth_identities` (`id`, `user_id`, `type`, `name`, `secret`, `secret2`, `expires`, `extra`, `force_reset`, `last_used_at`, `created_at`, `updated_at`) VALUES
-(1, 1, 'email_password', 'admin', 'admin@sgc.com', '$2y$12$ZOA2jp5oiL9rT4vxne4imuIal2HY/e4MNSPQEwZ9Hb6rRfmZ1jj9i', NULL, NULL, 0, '2024-01-03 09:16:47', '2023-12-14 11:16:21', '2024-01-03 09:16:47'),
-(12, 18, 'email_password', 'penjual', 'penjual@sgc.com', '$2y$12$YcIpMPDJnVrRpkb8RoJhJODOeIcnZcMP/IRmgxL9XtM8pPkj1cBl.', NULL, NULL, 0, '2024-01-03 09:15:11', '2023-12-29 04:17:46', '2024-01-03 09:15:11'),
-(13, 19, 'email_password', 'user', 'customer@sgc.com', '$2y$12$9tuwffpU1vp3i7PEpFs2UOD2ZOt1pnXAGhA4089QT.as1ssAjdH5C', NULL, NULL, 0, '2024-01-03 09:15:55', '2023-12-29 04:24:08', '2024-01-03 09:15:55');
+(1, 1, 'email_password', 'admin', 'admin@sgc.com', '$2y$12$ZOA2jp5oiL9rT4vxne4imuIal2HY/e4MNSPQEwZ9Hb6rRfmZ1jj9i', NULL, NULL, 0, '2024-01-10 18:13:21', '2023-12-14 11:16:21', '2024-01-10 18:13:21'),
+(12, 18, 'email_password', 'penjual', 'penjual@sgc.com', '$2y$12$YcIpMPDJnVrRpkb8RoJhJODOeIcnZcMP/IRmgxL9XtM8pPkj1cBl.', NULL, NULL, 0, '2024-01-10 13:49:20', '2023-12-29 04:17:46', '2024-01-10 13:49:20'),
+(13, 19, 'email_password', 'user', 'customer@sgc.com', '$2y$12$9tuwffpU1vp3i7PEpFs2UOD2ZOt1pnXAGhA4089QT.as1ssAjdH5C', NULL, NULL, 0, '2024-01-03 09:15:55', '2023-12-29 04:24:08', '2024-01-03 09:15:55'),
+(15, 21, 'email_password', NULL, 'cek@gmail.com', '$2y$12$HtiyUPr9k1eFrKd6aK54Le.SbUkNoOJd6CRVBeOVaVJcJrq6KUlli', NULL, NULL, 0, NULL, '2024-01-04 05:56:42', '2024-01-04 05:56:43'),
+(17, 23, 'email_password', NULL, 'penjual3@email.com', '$2y$12$YcIpMPDJnVrRpkb8RoJhJODOeIcnZcMP/IRmgxL9XtM8pPkj1cBl.', NULL, NULL, 0, '2024-01-10 17:53:16', '2024-01-10 14:44:15', '2024-01-10 17:53:16'),
+(18, 24, 'email_password', NULL, 'penjual2@email.com', '$2y$12$Al3D/xDAmu75oOwuFNqq3O/ASP4qlhAoVoTaVnEc7WEFWM6oD4FLy', NULL, NULL, 0, '2024-01-10 17:53:03', '2024-01-10 14:45:18', '2024-01-10 17:53:03');
 
 -- --------------------------------------------------------
 
@@ -88,7 +95,7 @@ CREATE TABLE `auth_logins` (
   `user_id` int(11) UNSIGNED DEFAULT NULL,
   `date` datetime NOT NULL,
   `success` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `auth_logins`
@@ -143,7 +150,21 @@ INSERT INTO `auth_logins` (`id`, `ip_address`, `user_agent`, `id_type`, `identif
 (46, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0', 'username', 'user', 19, '2024-01-03 09:14:38', 1),
 (47, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0', 'username', 'penjual', 18, '2024-01-03 09:15:11', 1),
 (48, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0', 'username', 'user', 19, '2024-01-03 09:15:55', 1),
-(49, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0', 'username', 'admin', 1, '2024-01-03 09:16:47', 1);
+(49, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0', 'username', 'admin', 1, '2024-01-03 09:16:47', 1),
+(50, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin', 1, '2024-01-04 07:28:59', 1),
+(51, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin13', NULL, '2024-01-07 03:26:42', 0),
+(52, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin', 1, '2024-01-07 03:27:13', 1),
+(53, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin', 1, '2024-01-10 13:37:15', 1),
+(54, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual', 18, '2024-01-10 13:49:20', 1),
+(55, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin', 1, '2024-01-10 14:36:54', 1),
+(56, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual2', NULL, '2024-01-10 14:45:50', 0),
+(57, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual2', NULL, '2024-01-10 14:45:57', 0),
+(58, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual3', NULL, '2024-01-10 14:46:05', 0),
+(59, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual2', 24, '2024-01-10 14:46:43', 1),
+(60, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual2', NULL, '2024-01-10 17:52:59', 0),
+(61, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual2', 24, '2024-01-10 17:53:03', 1),
+(62, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'penjual3', 23, '2024-01-10 17:53:16', 1),
+(63, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', 'username', 'admin', 1, '2024-01-10 18:13:21', 1);
 
 -- --------------------------------------------------------
 
@@ -156,7 +177,7 @@ CREATE TABLE `auth_permissions_users` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `permission` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -172,7 +193,7 @@ CREATE TABLE `auth_remember_tokens` (
   `expires` datetime NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -189,7 +210,7 @@ CREATE TABLE `auth_token_logins` (
   `user_id` int(11) UNSIGNED DEFAULT NULL,
   `date` datetime NOT NULL,
   `success` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -205,7 +226,7 @@ CREATE TABLE `migrations` (
   `namespace` varchar(255) NOT NULL,
   `time` int(11) NOT NULL,
   `batch` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `migrations`
@@ -225,10 +246,22 @@ INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`
 CREATE TABLE `produk_toko` (
   `id_produk` int(11) NOT NULL,
   `nama_produk` varchar(255) NOT NULL,
+  `harga_produk` int(25) NOT NULL,
+  `deskripsi_produk` text NOT NULL,
   `foto_produk` varchar(255) NOT NULL,
   `stok_produk` varchar(10) NOT NULL,
+  `tersedia` tinyint(1) NOT NULL,
   `id_toko` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `produk_toko`
+--
+
+INSERT INTO `produk_toko` (`id_produk`, `nama_produk`, `harga_produk`, `deskripsi_produk`, `foto_produk`, `stok_produk`, `tersedia`, `id_toko`) VALUES
+(2, 'Minyak Sunco 1L', 34000, 'Mahaaaalllllllllllllll', 'produk-1fc41a.jpg', '10', 1, 19),
+(3, 'Gulaku Gula Pasir 1Kg', 27000, 'Gulaa manissss', 'produk-2178b8.jpg', '13', 1, 19),
+(4, 'Seblak Mie Tulang', 12000, 'Seblak Mie dengan Mie Indomie, Tulang, telur dan Sayuran, Tersedia Toping Lain', 'default.png', '13', 0, 17);
 
 -- --------------------------------------------------------
 
@@ -245,7 +278,7 @@ CREATE TABLE `settings` (
   `context` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -255,20 +288,23 @@ CREATE TABLE `settings` (
 
 CREATE TABLE `toko` (
   `id_toko` int(11) NOT NULL,
-  `id_user` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED DEFAULT NULL,
   `nama_toko` varchar(255) NOT NULL,
   `alamat_toko` text NOT NULL,
-  `nohp_toko` int(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nohp_toko` varchar(13) NOT NULL,
+  `foto` text NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `toko`
 --
 
-INSERT INTO `toko` (`id_toko`, `id_user`, `nama_toko`, `alamat_toko`, `nohp_toko`) VALUES
-(5, 18, 'Srikandi', 'Blok C4', 8123),
-(6, 18, 'Gramedia', 'Blok A1', 8123),
-(8, 18, 'Kandita', 'Blok G9', 8123);
+INSERT INTO `toko` (`id_toko`, `user_id`, `nama_toko`, `alamat_toko`, `nohp_toko`, `foto`, `deskripsi`) VALUES
+(6, 1, 'Gramedia SBG', 'Blok A1', '8123', 'default.svg', '  daweawd'),
+(8, 18, 'Kandita Super', 'Blok G9', '2147483647', 'unnamed.jpg', ''),
+(17, 23, 'Seblak Ganas', 'C12', '08216958452', 'default.svg', 'Nyiksa Perut? Gaskkan'),
+(19, 24, 'Sembako Murah 179', 'C19', '085155296228', 'toko-804ef2.jpg', 'Sembako Termurah dengan diskon besar');
 
 -- --------------------------------------------------------
 
@@ -288,16 +324,19 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `fullname`, `user_image`, `status`, `status_message`, `active`, `last_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'admin', 'Wawan', 'user-6587a083011e7.jpg', NULL, NULL, 1, '2024-01-03 09:16:47', '2023-12-14 11:16:20', '2023-12-24 04:48:47', NULL),
-(18, 'penjual', 'Rere', '', NULL, NULL, 1, '2024-01-03 09:15:16', '2023-12-29 04:17:46', '2023-12-29 04:17:46', NULL),
-(19, 'user', 'Dede', '', NULL, NULL, 1, '2024-01-03 09:16:41', '2023-12-29 04:24:08', '2023-12-29 04:24:08', NULL);
+(1, 'admin', 'Wawan', 'user-6587a083011e7.jpg', NULL, NULL, 1, '2024-01-10 18:14:26', '2023-12-14 11:16:20', '2023-12-24 04:48:47', NULL),
+(18, 'penjual', 'Rere', '', NULL, NULL, 1, '2024-01-10 14:36:49', '2023-12-29 04:17:46', '2023-12-29 04:17:46', NULL),
+(19, 'user', 'Dede', '', NULL, NULL, 1, '2024-01-03 09:16:41', '2023-12-29 04:24:08', '2023-12-29 04:24:08', NULL),
+(21, 'cek', NULL, 'default.svg', NULL, NULL, 1, NULL, '2024-01-04 05:56:42', '2024-01-10 13:49:06', '2024-01-10 13:49:06'),
+(23, 'penjual3', 'Leonidas', 'default.svg', NULL, NULL, 1, '2024-01-10 18:11:46', '2024-01-10 14:44:15', '2024-01-10 17:53:31', NULL),
+(24, 'penjual2', 'Amin Suprata', '1704901921_1b7e32f919437ac186f0.jpg', NULL, NULL, 1, '2024-01-10 17:53:07', '2024-01-10 14:45:18', '2024-01-10 15:52:01', NULL);
 
 --
 -- Indexes for dumped tables
@@ -373,7 +412,7 @@ ALTER TABLE `settings`
 --
 ALTER TABLE `toko`
   ADD PRIMARY KEY (`id_toko`),
-  ADD KEY `id_user` (`id_user`) USING BTREE;
+  ADD KEY `id_user` (`user_id`) USING BTREE;
 
 --
 -- Indeks untuk tabel `users`
@@ -390,19 +429,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `auth_groups_users`
 --
 ALTER TABLE `auth_groups_users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_identities`
 --
 ALTER TABLE `auth_identities`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_logins`
 --
 ALTER TABLE `auth_logins`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT untuk tabel `auth_permissions_users`
@@ -432,7 +471,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `produk_toko`
 --
 ALTER TABLE `produk_toko`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `settings`
@@ -444,13 +483,13 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT untuk tabel `toko`
 --
 ALTER TABLE `toko`
-  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -490,7 +529,7 @@ ALTER TABLE `produk_toko`
 -- Ketidakleluasaan untuk tabel `toko`
 --
 ALTER TABLE `toko`
-  ADD CONSTRAINT `toko_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `toko_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
